3bca0d059b324d3c9abdb4c73b52f1b1712bcde6
TheNephalian <salseethrough@gmail.com>
2022-01-19 17:45:14 -0600

    Added some tests to see if the function works as intended. Tests appear satisfactory.

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from offensiveCR import cal_Offensive_CR
 
-
-print(cal_Offensive_CR(210))
+'''Testing of functions'''
+print(cal_Offensive_CR(2)); '''Should return 1/4'''
+print(cal_Offensive_CR(21)); '''Should return 3'''
+print(cal_Offensive_CR(210)); '''Should return 24'''
+print(cal_Offensive_CR(410)); '''Should print error message 'Damage too high'. Also, returns None as no value is actually returned by cal_Offensive_CR'''

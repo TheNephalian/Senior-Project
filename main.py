9da067ef32a1833acb82c9d3af4e7e5e80a79d72
TheNephalian <salseethrough@gmail.com>
2022-01-21 09:52:26 -0600

    Coding and Editing of Offensive Challenge Rating Calculations

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
-from offensiveCR import cal_Offensive_CR
+from offensiveCR import cal_pros_offensive_CR
+from offensiveCR import cal_pros_prof_bns
 
 '''Testing of functions'''
-print(cal_Offensive_CR(2)); '''Should return 1/4'''
-print(cal_Offensive_CR(21)); '''Should return 3'''
-print(cal_Offensive_CR(210)); '''Should return 24'''
-print(cal_Offensive_CR(410)); '''Should print error message 'Damage too high'. Also, returns None as no value is actually returned by cal_Offensive_CR'''
+print("***Testing cal_pros_offensive_CR***")
+print(cal_pros_offensive_CR(2)); '''Should print 1/4'''
+print(cal_pros_offensive_CR(21)); '''Should print 3'''
+print(cal_pros_offensive_CR(210)); '''Should print 24'''
+print(cal_pros_offensive_CR(410)); '''Should print error message. Also, prints 'None' as no value is actually returned by cal_offensive_CR'''
+
+print()
+
+print("***Testing cal_pros_prof_bns***")
+print(cal_pros_prof_bns(0)); '''Should print 2'''
+print(cal_pros_prof_bns(2)); '''Should print 2'''
+print(cal_pros_prof_bns(4)); '''Should print 2'''
+print(cal_pros_prof_bns(6)); '''Should print 3'''
+print(cal_pros_prof_bns(13)); '''Should print 5'''
+print(cal_pros_prof_bns(20)); '''Should print 6'''
+print(cal_pros_prof_bns(26)); '''Should print 8'''
+print(cal_pros_prof_bns(29)); '''Should print 9'''
+print(cal_pros_prof_bns(33)); '''Should print error message. Also, prints 'None' as no value is actually returned by cal_pros_prof_bns'''
--- offensiveCR.py
+++ offensiveCR.py
@@ -1,150 +1,201 @@
-'''Function that calculates the Offensive Challenge Rating (CR) of the creature based on its damage per round (dmg_per_rnd)'''
-'''	This is the first step of calculating a creature's overall CR, '''
-''' 	which is to calculate its Offensive CR'''
-'''	Returns the prospective Offensive CR (pros_Off_CR), to be used in further calculation'''
-def cal_Offensive_CR(dmg_per_rnd):
+'''Function that calculates the prospecteve Offensive Challenge Rating (pros_off_CR) of the creature based on its damage per round (dmg_per_rnd)
+	This is the first step of calculating a creature's overall CR, which is to calculate its Offensive CR
+
+	dmg_per_round must be a value between 0-320
+	Returns pros_Off_CR'''
+def cal_pros_offensive_CR(dmg_per_rnd):
 	if (dmg_per_rnd <= 1):
-		pros_Off_CR = 0
-		return pros_Off_CR
+		pros_off_CR = 0
+		return pros_off_CR
 
 	elif (dmg_per_rnd <= 3):
-		pros_Off_CR = 1/8
-		return pros_Off_CR
+		pros_off_CR = 1/8
+		return pros_off_CR
 
 	elif (dmg_per_rnd <= 5):
-		pros_Off_CR = 1/4
-		return pros_Off_CR
+		pros_off_CR = 1/4
+		return pros_off_CR
 
 	elif (dmg_per_rnd<= 8):
-		pros_Off_CR = 1/2
-		return pros_Off_CR
+		pros_off_CR = 1/2
+		return pros_off_CR
 
 	elif (dmg_per_rnd <= 14):
-		pros_Off_CR = 1
-		return pros_Off_CR
+		pros_off_CR = 1
+		return pros_off_CR
 
 	elif (dmg_per_rnd <= 14):
-		pros_Off_CR = 1
-		return pros_Off_CR
+		pros_off_CR = 1
+		return pros_off_CR
 
 	elif (dmg_per_rnd <= 20):
-		pros_Off_CR = 2
-		return pros_Off_CR
+		pros_off_CR = 2
+		return pros_off_CR
 
 	elif (dmg_per_rnd <= 26):
-		pros_Off_CR = 3
-		return pros_Off_CR
+		pros_off_CR = 3
+		return pros_off_CR
 
 	elif (dmg_per_rnd <= 32):
-		pros_Off_CR = 4
-		return pros_Off_CR
+		pros_off_CR = 4
+		return pros_off_CR
 
 	elif (dmg_per_rnd <= 38):
-		pros_Off_CR = 5
-		return pros_Off_CR
+		pros_off_CR = 5
+		return pros_off_CR
 
 	elif (dmg_per_rnd <= 44):
-		pros_Off_CR = 6
-		return pros_Off_CR
+		pros_off_CR = 6
+		return pros_off_CR
 
 	elif (dmg_per_rnd <= 50):
-		pros_Off_CR = 7
-		return pros_Off_CR
+		pros_off_CR = 7
+		return pros_off_CR
 
 	elif (dmg_per_rnd <= 56):
-		pros_Off_CR = 8
-		return pros_Off_CR
+		pros_off_CR = 8
+		return pros_off_CR
 
 	elif (dmg_per_rnd <= 62):
-		pros_Off_CR = 9
-		return pros_Off_CR
+		pros_off_CR = 9
+		return pros_off_CR
 
 	elif (dmg_per_rnd <= 68):
-		pros_Off_CR = 10
-		return pros_Off_CR
+		pros_off_CR = 10
+		return pros_off_CR
 
 	elif (dmg_per_rnd <= 74):
-		pros_Off_CR = 11
-		return pros_Off_CR
+		pros_off_CR = 11
+		return pros_off_CR
 
 	elif (dmg_per_rnd <= 80):
-		pros_Off_CR = 12
-		return pros_Off_CR
+		pros_off_CR = 12
+		return pros_off_CR
 
 	elif (dmg_per_rnd <= 86):
-		pros_Off_CR = 13
-		return pros_Off_CR
+		pros_off_CR = 13
+		return pros_off_CR
 
 	elif (dmg_per_rnd <= 92):
-		pros_Off_CR = 14
-		return pros_Off_CR
+		pros_off_CR = 14
+		return pros_off_CR
 
 	elif (dmg_per_rnd <= 98):
-		pros_Off_CR = 15
-		return pros_Off_CR
+		pros_off_CR = 15
+		return pros_off_CR
 
 	elif (dmg_per_rnd <= 104):
-		pros_Off_CR = 16
-		return pros_Off_CR
+		pros_off_CR = 16
+		return pros_off_CR
 
 	elif (dmg_per_rnd <= 110):
-		pros_Off_CR = 17
-		return pros_Off_CR
+		pros_off_CR = 17
+		return pros_off_CR
 
 	elif (dmg_per_rnd <= 116):
-		pros_Off_CR = 18
-		return pros_Off_CR
+		pros_off_CR = 18
+		return pros_off_CR
 
 	elif (dmg_per_rnd <= 122):
-		pros_Off_CR = 19
-		return pros_Off_CR
+		pros_off_CR = 19
+		return pros_off_CR
 
 	elif (dmg_per_rnd <= 140):
-		pros_Off_CR = 20
-		return pros_Off_CR
+		pros_off_CR = 20
+		return pros_off_CR
 
 	elif (dmg_per_rnd <= 158):
-		pros_Off_CR = 21
-		return pros_Off_CR
+		pros_off_CR = 21
+		return pros_off_CR
 
 	elif (dmg_per_rnd <= 176):
-		pros_Off_CR = 22
-		return pros_Off_CR
+		pros_off_CR = 22
+		return pros_off_CR
 
 	elif (dmg_per_rnd <= 194):
-		pros_Off_CR = 23
-		return pros_Off_CR
+		pros_off_CR = 23
+		return pros_off_CR
 
 	elif (dmg_per_rnd <= 212):
-		pros_Off_CR = 24
-		return pros_Off_CR
+		pros_off_CR = 24
+		return pros_off_CR
 
 	elif (dmg_per_rnd <= 230):
-		pros_Off_CR = 25
-		return pros_Off_CR
+		pros_off_CR = 25
+		return pros_off_CR
 
 	elif (dmg_per_rnd <= 248):
-		pros_Off_CR = 26
-		return pros_Off_CR
+		pros_off_CR = 26
+		return pros_off_CR
 
 	elif (dmg_per_rnd <= 266):
-		pros_Off_CR = 27
-		return pros_Off_CR
+		pros_off_CR = 27
+		return pros_off_CR
 
 	elif (dmg_per_rnd <= 284):
-		pros_Off_CR = 28
-		return pros_Off_CR
+		pros_off_CR = 28
+		return pros_off_CR
 
 	elif (dmg_per_rnd <= 302):
-		pros_Off_CR = 29
-		return pros_Off_CR
+		pros_off_CR = 29
+		return pros_off_CR
 
 	elif (dmg_per_rnd <= 320):
-		pros_Off_CR = 30
-		return pros_Off_CR
-
-	elif (dmg_per_rnd > 320):
-		print("Damage per round is too high. Damage must be within 0-320.")
+		pros_off_CR = 30
+		return pros_off_CR
 
 	else:
-		print("Error when reading Damage per Round.")
+		print("Error. Damage must be within 0-320.")
+
+'''Function that calculates the prospected proficiency bonus (pros_prof_bns) of the creature based on its expected Challenge Rating (CR)
+	Expected CR is input by the user and used to calculate initial proficiency bonuses to the creature
+
+	expted_CR must be a value between 0-30
+	Returns pros_prof_bns'''
+def cal_pros_prof_bns(exptd_CR):
+	if (exptd_CR in range(0, 5)):
+		pros_prof_bns = 2
+		return pros_prof_bns
+
+	elif (exptd_CR in range(5, 9)):
+		pros_prof_bns = 3
+		return pros_prof_bns
+
+	elif (exptd_CR in range(9, 13)):
+		pros_prof_bns = 4
+		return pros_prof_bns
+
+	elif (exptd_CR in range(13, 17)):
+		pros_prof_bns = 5
+		return pros_prof_bns
+
+	elif (exptd_CR in range(17, 21)):
+		pros_prof_bns = 6
+		return pros_prof_bns
+
+	elif (exptd_CR in range(21, 25)):
+		pros_prof_bns = 7
+		return pros_prof_bns
+
+	elif (exptd_CR in range(25, 29)):
+		pros_prof_bns = 8
+		return pros_prof_bns
+
+	elif (exptd_CR in range(29, 31)):
+		pros_prof_bns = 5
+		return pros_prof_bns
+
+	else:
+		print("Error. Expected Challenge Rating must be between 0-30.")
+
+'''Function that calculates the monster's prospective attack bonus (pros_atk_bns) for melee or ranged attacks
+	This requires several parameters:
+		the prospective proficiency bonus (pros_prof_bns)
+		the creature's Strength (STR) or
+		the creature's Dexterity (DEX)
+		and whether the attack uses the creature's STR or DEX (str_or_dex)
+	
+	Returns pros_atk_bns'''
+def cal_pros_atk_bns(exptd_CR, STR, DEX, str_or_dex):
+	if (str_or_dex == "STR"):
+		return

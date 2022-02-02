b34d0790e59f672098d3c070bf745fa93ebf3808
TheNephalian <salseethrough@gmail.com>
2022-02-02 17:07:40 -0600

    Added notes for further defense CR calc's

---
+++ defenseInputs.py
@@ -0,0 +1,54 @@
+'''
+Size - Die - hp/die:
+Tiny - d4 - 2.5 hp
+Small - d6 - 3.5
+Medium - d8 - 4.5
+Large - d10 - 5.5
+Huge - d12 - 6.5
+Gargantuan - d20 - 10.5
+
+Constitution Attribute Range - Hp bonus/die:
+
+0-1 - -5
+2-3 - -4
+4-5 - -3
+6-7 - -2
+8-9 - -1
+10-11 - +0
+12-13 - +1
+14-15 - +2
+16-17 - +3
+18-19 - +4
+20-21 - +5
+22... +6
+24... +7
+26... +8
+28... +9
+30+ - +10
+
+Example:
+Large, d10
+10d10
+From stats, 15 Constitution
+
+Hp = 10 * 5.5 + 10 * 2 = 75
+
+Medium, d8
+4d8
+From stats, 7 Constitution
+
+Hp = 4 * 4.5 + 4 * (-2) = 10
+'''
+
+'''
+Vulnerabilities
+If a creature has any, it's hp are 'effectively' halved
+'''
+
+'''
+Save Proficiencies
+Up to 6 total
+
+AC is 'effectively' increased by 2 if the creature has 3 or 4,
+	'effectively' increased by 4 if 5 or 6
+'''

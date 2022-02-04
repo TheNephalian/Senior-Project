51879601374cacebd6e5995e2c562dfaf0c9c325
MintyMintTea <72091651+MintyMintTea@users.noreply.github.com>
2022-02-04 17:05:15 -0600

    Updates to UI

--- main.py
+++ main.py
@@ -10,12 +10,12 @@ function_test()
 
 print(cal_crtr_CR(100, 15, 19, False, 18, 250))
 
-def window():
-    app = QApplication(sys.argv)
-    win = QMainWindow()
-    win.setGeometry(320,160,1280,720)
-    win.setWindowTitle("2022 Senior project")
+# def window():
+#     app = QApplication(sys.argv)
+#     win = QMainWindow()
+#     win.setGeometry(320,160,1280,720)
+#     win.setWindowTitle("2022 Senior project")
 
-    win.show()
-    sys.exit(app.exec_())
-window()
+#     win.show()
+#     sys.exit(app.exec_())
+# window()

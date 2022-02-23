c1b3ba597ab0c9435ae343c72ff0ef3990efc9a3
TheNephalian <77940031+TheNephalian@users.noreply.github.com>
2022-02-22 19:09:51 -0600

    Notes for Jose, starting of dummy creatures

--- creature.py
+++ creature.py
@@ -1,13 +1,8 @@
-import math
-
 class myCreature:
     def __init__(inputSize, die, hp, size):
         inputSize.die = die
         inputSize.hp = hp
         inputSize.size = size
+    
     def myfun(inputSize):
         print(inputSize.size)
-    
-
-
-        
---
+++ terrasque.py
@@ -0,0 +1,6 @@
+from creature import myCreature
+
+
+class terrasque (myCreature):
+	def __init__(self):
+		myCreature.__init__
